0b5abd7208c49836a273d5145fc5a4b514192c7e
Rene <r.lutz01@web.de>
2018-08-11 16:21:02 +0200

    Added keyboard control for Professor

--- scripts/Professor.gd
+++ scripts/Professor.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var speed = 300
+var vel = Vector2()
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_fixed_process(true)
+	
+func _fixed_process(delta):
+	var input = Vector2(0, 0)
+	input.x = Input.is_action_pressed("ui_right") - Input.is_action_pressed("ui_left")
+	vel = input.normalized() * speed
+	var pos = get_pos() + vel * delta
+	set_pos(pos)

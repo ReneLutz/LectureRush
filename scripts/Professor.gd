19312ef266069f26b9c68c38c2fad50110b98e9e
Rene <r.lutz01@web.de>
2018-08-11 19:42:30 +0200

    Added movement animation on key-press. Also changed node type of professor to kinematic node

--- scripts/Professor.gd
+++ scripts/Professor.gd
@@ -1,8 +1,12 @@
 extends KinematicBody2D
 
+enum { STANDING, WALKING }
 const WALK_SPEED = 80
 
+onready var sprite = get_node("AnimatedSprite")
+
 var velocity = Vector2()
+var animation = STANDING
 
 func _ready():
 	set_fixed_process(true)
@@ -11,12 +15,20 @@ func _fixed_process(delta):
 	
 	if (Input.is_key_pressed(KEY_A)):
 		velocity.x = -WALK_SPEED
+		animation = WALKING
 	elif (Input.is_key_pressed(KEY_D)):
 		velocity.x =  WALK_SPEED
+		animation = WALKING
 	else:
 		velocity.x = 0
+		animation = STANDING
 	
 	var motion = velocity * delta
+	
+	if(animation == STANDING):
+		sprite.play("Idle")
+	else:
+		sprite.play("Walking")
 	move(motion)
 	
 #func :

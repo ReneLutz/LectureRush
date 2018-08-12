05f777861de479e939b8679ac2b4c651c2b78ecf
Rene <rene.lutz@stud.tu-darmstadt.de>
2018-08-12 21:00:56 +0200

    Added lecture countdown and adapted tilemap to new width

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,14 +1,19 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	set_fixed_process(true)
 	get_node("Professor").get_node("profBody").setPanicUI(get_node("Panic"))
-
-
+	
+func _fixed_process(delta):
+	var uiTimeLabel = get_node("UI/Time")
+	var lectureTimer = get_node("LectureTimer")
+	
+	var remainingTime = lectureTimer.get_time_left()
+	var minutes = int(remainingTime) / 60
+	var seconds = int(remainingTime) % 60
+	
+	uiTimeLabel.set_text(str(minutes) + ":" + str(seconds))
+	
 func _on_LectureTimer_timeout():
+	# End game, because lecture is over
 	pass # replace with function body

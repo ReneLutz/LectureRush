af0a43c49023785be6d43c06beb448941f75d53f
Minakon <johanna-rahm@gmx.de>
2018-08-12 11:03:36 +0200

    Decoration Choices

--- scripts/Decoration.gd
+++ scripts/Decoration.gd
@@ -3,8 +3,29 @@ extends Node
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var decoration_themes = [["Starry Night"], ["Periodic Table", "Chemistry Rack"]]
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	pass
+	theme_choice()
+
+func theme_choice():
+	randomize()
+	var theme_index = randi() % decoration_themes.size()
+	var theme = decoration_themes[theme_index]
+	for item in theme:
+		get_node(item).show()
+
+
+
+
+
+
+
+#for i in arts_decoration 
+#	get node visible 
+#for y in chemistry_dec
+#	get node unvisible
+
+#print(arts_decoration)

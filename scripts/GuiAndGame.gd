a627b2fc7b4aa261ee4188f472e152799e56fcae
Lars Lotter <larslotter@googlemail.com>
2018-08-13 19:15:55 +0200

    resetscreen

--- scripts/GuiAndGame.gd
+++ scripts/GuiAndGame.gd
@@ -5,14 +5,20 @@ extends Node
 # var b = "textvar"
 
 const Game = preload("res://scenes/levels/Game.tscn")
+var currentGameInstance = null
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	get_node("Control/Button").connect("button_up",self,"playPressed")
 
+func endGame():
+	remove_child(currentGameInstance)
+	currentGameInstance = null
+	get_node("Control").show()
+
 func playPressed():
-	print("pressed")
-	var game = Game.instance()
+	currentGameInstance = Game.instance()
+	currentGameInstance.connect("gameEnded", self, "endGame")
 	get_node("Control").hide()
-	add_child(game)
+	add_child(currentGameInstance)
